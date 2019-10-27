24b0298599aaf596ee743c533b2a559e2ce69b68
William Stevens <wstevens.hornet@gmail.com>
2019-10-27 23:33:17 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,14 +1,14 @@
-class Backer 
+class Backer
   
-  attr_accessor: :name, :projects
-  
+  attr_accessor :name, :backed_projects
+
   def initialize(name)
-    @name = name 
-    @backedprojects = []
+    @name = name
+    @backed_projects = []
   end
-  
+
   def back_project(project)
-    @projects << project
+    @backed_projects << project
     project.add_backer(self) unless project.backers.include?(self)
   end
   
